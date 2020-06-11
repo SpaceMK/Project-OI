50224d5d673cd4dc9425550ab915cf8481ecce7e
SpaceUK <spaso.ilievski@live.com>
2020-06-11 17:09:50 +0100

    Final lighting added

--- Project-OI/Assets/Scripts/Player/PlayerObjectInteraction.cs
+++ Project-OI/Assets/Scripts/Player/PlayerObjectInteraction.cs
@@ -1,7 +1,5 @@
 ﻿using ServiceLocatorSample.ServiceLocator;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerObjectInteraction : MonoBehaviour
---
+++ Project-OI/Assets/Scripts/SceneControllers/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using ServiceLocatorSample.ServiceLocator;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        CreateServiceLocator();    
+    }
+
+    void CreateServiceLocator()
+    {
+        ServiceLocator.Initiailze();
+    }
+}
